ac24169654821abcf3dea9e64599c3a5059f5d9c
papeixoto <pazevedopeixoto@gmail.com>
2022-06-16 18:44:12 +0100

    change paths again

--- web/pages/posts/[id].js
+++ web/pages/posts/[id].js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Layout from "../../components/layout";
-import Date from "../../components/date";
-import { getAllPostIds, getPostData } from "../../lib/posts";
-import utilStyles from "../../styles/utils.module.css";
+import Layout from "/components/layout";
+import Date from "/components/date";
+import { getAllPostIds, getPostData } from "/lib/posts";
+import utilStyles from "/styles/utils.module.css";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
